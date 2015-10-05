b6b4858a2cf2a7199451c5540feba3adec1b41de
Loren Yeung <loren.w.yeung@gmail.com>
2015-10-04 20:29:28 -0700

    signup and password for android app

--- Apps/Android/Slugging/app/src/main/java/carpooltunnel/slugging/MainActivity.java
+++ Apps/Android/Slugging/app/src/main/java/carpooltunnel/slugging/MainActivity.java
@@ -1,7 +1,8 @@
 package carpooltunnel.slugging;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,36 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Parse.enableLocalDatastore(this);
+        //Parse.initialize(this, "VPZjlR7XLjBjn7KPCiCgaAexMUSX13fNe2wZ4mps", "lMHGifcMpdu9yKDL5YFT1Rw79fo466o7BngcneUF");
+
+//        ParseUser user = new ParseUser();
+//        user.setUsername("my name");
+//        user.setPassword("my pass");
+//        user.setEmail("email@example.com");
+//
+//// other fields can be set just like with ParseObject
+//        user.put("phone", "650-555-0000");
+//
+//        user.signUpInBackground(new SignUpCallback() {
+//            public void done(com.parse.ParseException e) {
+//                if (e == null) {
+//                    // Hooray! Let them use the app now.
+//                } else {
+//                    // Sign up didn't succeed. Look at the ParseException
+//                    // to figure out what went wrong
+//                }
+//            }
+//        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this,LoginActivity.class); //go to login
+        startActivity(intent);
+        finish();
+
     }
 
     @Override

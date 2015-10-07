1bc3ff6651b164dcfb7699c59f3b167a315abb9a
Karney Tao <ktao@ucsc.edu>
2015-10-07 16:31:57 -0700

    Added TimeSelector popup on Time Box click (needs to click twice on first time for some reason), added ParseObject Subclass called ParseRoutes(stores routes), Submit on Driver View now submits inputted route.

--- Apps/Android/Slugging/app/src/main/java/carpooltunnel/slugging/MyApplication.java
+++ Apps/Android/Slugging/app/src/main/java/carpooltunnel/slugging/MyApplication.java
@@ -2,11 +2,12 @@ package carpooltunnel.slugging;
 
 import android.app.Application;
 import com.parse.Parse;
-
+import com.parse.ParseObject;
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        ParseObject.registerSubclass(ParseRoute.class);
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "VPZjlR7XLjBjn7KPCiCgaAexMUSX13fNe2wZ4mps", "lMHGifcMpdu9yKDL5YFT1Rw79fo466o7BngcneUF");
     }

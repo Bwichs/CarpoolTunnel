ced2e9537380b8cb9c26bf9269521dce32383846
Brian Wichers <bwichers@ucsc.edu>
2015-10-29 16:27:13 -0700

    Implementing MapKit code with April

--- Apps/iOS/Slugging/ParseStarterProject/PassengerViewController.swift
+++ Apps/iOS/Slugging/ParseStarterProject/PassengerViewController.swift
@@ -7,13 +7,58 @@
 //
 
 import UIKit
+import MapKit
 import Parse
 
-class PassengerViewController: UIViewController {
+@available(iOS 8.0, *)
+class PassengerViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let regionRadius: CLLocationDistance = 1000
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func addPin(location: CLLocation){
+        let dropPin = MKPointAnnotation()
+        dropPin.coordinate = location.coordinate
+        dropPin.title = "UCSC"
+        mapView.addAnnotation(dropPin)
+    }
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.mapView.delegate = self
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        if let userLocation: CLLocation! = locationManager.location {
+            centerMapOnLocation(userLocation!)
+        }
+
+        
+        //map view center init
+        let initialLocation = CLLocation(latitude: 37.000000, longitude: -122.060000)
+        
+        addPin(initialLocation)
+        
+
+        
+        //Set parse from and to data to map routes
+            
+        
+        //done with setting route on map*/
+        
+        
 
         // Do any additional setup after loading the view.
         if self.revealViewController() != nil {
